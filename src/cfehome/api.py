46a58442d38e9140dac3f171769fb8d82899c565
Viktor Rura <ruravictor@gmail.com>
2024-07-07 16:52:06 +0100

    Add JWT auth

--- src/cfehome/api.py
+++ src/cfehome/api.py
@@ -1,6 +1,11 @@
 from ninja import NinjaAPI, Schema
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
 
-api = NinjaAPI()
+
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
 
 
 class UserSchema(Schema):
@@ -16,7 +21,7 @@ def hello(request):
     return "hello world"
 
 
-@api.get("/me", response=UserSchema)
+@api.get("/me", response=UserSchema, auth=JWTAuth())
 def me(request):
     print(request)
     return request.user
